95ab1838b59ef9ce172c722ec3f1878f700dfce4
Arvit Varfaj <avarfaj86@gmail.com>
2021-11-19 10:46:25 +0100

    first commit

---
+++ src/main/java/com/cognizant/core/domain/Customer.java
@@ -0,0 +1,27 @@
+package com.cognizant.core.domain;
+
+public class Customer {
+    String id;
+    String firstName;
+    String lastName;
+
+    public Customer() { }
+
+    public Customer(String id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
+
+    public String getFirstName() { return firstName; }
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+}
